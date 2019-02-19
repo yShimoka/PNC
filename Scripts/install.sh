e5371f2910eb3c271b6eca23a9926276c2d8d54c
Yasuo Shimoka <jeanbapti06@hotmail.fr>
2019-02-19 21:11:08 +0100

    Finished Continuous Deployment
    Added code for continuous deployment.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Install make and cmake.
-sudo apt-get update; sudo apt-get install make cmake -y;
+# Install zip, make and cmake.
+sudo apt-get update; sudo apt-get install make cmake zip -y;
 
 # Call the installer script.
 ./Scripts/Installers/${PLATFORM}-${ARCH}.sh
