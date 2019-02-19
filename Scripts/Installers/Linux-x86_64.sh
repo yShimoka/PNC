fc8f7805607f42b1ac03a2106713c269df6a578b
Yasuo Shimoka <jeanbapti06@hotmail.fr>
2019-02-19 20:46:40 +0100

    Added SDL2 dependencies
    Added required SDL2 dependencies for Linux compilation.

--- Scripts/Installers/Linux-x86_64.sh
+++ Scripts/Installers/Linux-x86_64.sh
@@ -3,11 +3,16 @@
 sudo apt-get update
 
 # Install gcc
-sudo apt-get install gcc
+sudo apt-get install gcc -y
+
+# Install sdl2 dependencies
+sudo apt-get install \
+    libgles2-mesa-dev \
+    libegl1-mesa-dev -y
 
 # Install the sdl2 library.
 sudo apt-get install \
     libsdl2-dev \
     libsdl2-ttf-dev \
     libsdl2-image-dev \
-    libsdl2-mixer-dev
+    libsdl2-mixer-dev -y
