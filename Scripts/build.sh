e1fa7388af2f0b804055de68aec6ee0073714706
Yasuo Shimoka <jeanbapti06@hotmail.fr>
2019-02-19 21:41:14 +0100

    Changed package naming convention.
    Changed x.x_x to x_x_x for uncompressing name-sake.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,5 +8,5 @@ make
 mkdir -p Build/out
 cp -r Assets Build/bin/
 cd Build/bin
-zip -r ../out/PNC-${MAJOR}.${MINOR}_${TRAVIS_BUILD_NUMBER}-${PLATFORM}_${ARCH}.zip *
+zip -r ../out/PNC-${MAJOR}_${MINOR}_${TRAVIS_BUILD_NUMBER}-${PLATFORM}_${ARCH}.zip *
 cd ../..
