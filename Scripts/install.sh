45398763fcfdc31915e0fc637790fcd53412bb4e
Yasuo Shimoka <git_commit@jb-caillaud.fr>
2019-04-21 21:47:11 +0200

    Updated install script for macOS.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-# Install zip, make and cmake.
-sudo apt-get update; sudo apt-get install make cmake zip -y;
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Install zip, make and cmake.
+    sudo apt-get update; sudo apt-get install make cmake zip -y;
+fi
 
 # Call the installer script.
 ./Scripts/Installers/${PLATFORM}-${ARCH}.sh
